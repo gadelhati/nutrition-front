ccddc9da14cb11b23eb1eceff150d9d06902e846
GadelhaTI <gadelha.ti@gmail.com>
2023-02-17 02:09:58 -0300

    send post to back

--- src/assets/api/api.tsx
+++ src/assets/api/api.tsx
@@ -4,8 +4,9 @@ import { getLocalAccessToken } from "../../service/service.token"
 const token = getLocalAccessToken();
 
 export const api = axios.create({
-  baseURL: "http://localhost:3119",
+  baseURL: "http://localhost:3119/nutrition-back",
   // baseURL: "https://10.5.193.20/application-back",
+  // headers: { 'content-type': 'application/json' }
 });
 
 api.interceptors.request.use(async config => {
--- src/component/user/user.signin.tsx
+++ src/component/user/user.signin.tsx
@@ -3,6 +3,8 @@ import { create } from "../../service/service"
 import { User } from "./user.interface";
 import { initialUser } from './user.initial';
 import { ErrorMessage } from '../../assets/error/errorMessage';
+import axios from 'axios';
+import { api } from '../../assets/api/api';
 
 export const UserSignin = () => {
     const [state, setState] = useState<User>(initialUser)
@@ -10,19 +12,32 @@ export const UserSignin = () => {
     const resetItem = () => {
         setState(initialUser)
     }
-    const createUser = async() => {
-        try {
-            const { data } = await create<User>("/user", state);
-        } catch(error: any) {
-            var errorMessage: ErrorMessage[] = []
-            if (error.response.data.errors != undefined) {
-                error.response.data.errors.forEach((element: any) => {
-                    errorMessage.push({ field: element.field, defaultMessage: [element.defaultMessage] })
-                })
-            } else {
-                errorMessage.push({ field: error.response.data.status, defaultMessage: [error.response.data]})
-            }
-        }
+    const zero = async() => {
+        let res = await api.post('/auth/login', state)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+        // https://axios-http.com/ptbr/docs/req_config
+        // https://zetcode.com/javascript/axios/
+        // https://www.atatus.com/blog/how-to-perform-http-requests-with-axios-a-complete-guide/
+        // https://blog.logrocket.com/using-axios-set-request-headers/
+    }
+    const login = async() => {
+        // try {
+            let response = await create<User>("/auth/login", state);
+        // } catch(error: any) {
+        //     var errorMessage: ErrorMessage[] = []
+        //     if (error.response.data.errors != undefined) {
+        //         error.response.data.errors.forEach((element: any) => {
+        //             errorMessage.push({ field: element.field, defaultMessage: [element.defaultMessage] })
+        //         })
+        //     } else {
+        //         errorMessage.push({ field: error.response.data.status, defaultMessage: [error.response.data]})
+        //     }
+        // }
     }
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
@@ -57,8 +72,9 @@ export const UserSignin = () => {
                     />
                     <label htmlFor="password">Password</label>
                 </div>
-                <button onClick={resetItem} className="w-20 btn btn-secondary">Reset</button>
-                <button onClick={createUser} className="w-20 btn btn-primary">Signin</button>
+                <button onClick={resetItem}>Reset</button>
+                <button onClick={zero}>zero</button>
+                <button onClick={login}>login</button>
                 {/* {loading && <>Loading...</>}
                 {error != null && JSON.stringify(error)} */}
             </article>
