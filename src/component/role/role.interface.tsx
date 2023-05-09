3c28751ddd9868bf6731ffcba839a925065d9c9e
GadelhaTI <gadelha.ti@gmail.com>
2023-05-08 22:16:17 -0300

    add privileges

--- src/component/role/role.interface.tsx
+++ src/component/role/role.interface.tsx
@@ -3,5 +3,5 @@ import { Privilege } from "../privilege/privilege.interface";
 export interface Role {
     id: string,
     name: string,
-    // privileges: Privilege[]
+    privileges: Privilege[]
 }
