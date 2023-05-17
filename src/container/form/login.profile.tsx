86f115b096246a1ccd01151682379a8fd13e4c89
GadelhaTI <gadelha.ti@gmail.com>
2023-05-16 23:18:57 -0300

    logout on profile

--- src/container/form/login.profile.tsx
+++ src/container/form/login.profile.tsx
@@ -67,7 +67,7 @@ export const LoginProfile = () => {
             {isValidToken() ?
                 <>
                     {getPayload().sub}{getRoles()}
-                    {!isValidToken() && <Button onClick={logoutUser}>Logout</Button>}
+                    {isValidToken() && <Button onClick={logoutUser}>Logout</Button>}
                 </>
                 :
                 <CenterItem>
