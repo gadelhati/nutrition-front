d0a46a58b1c50fcf9697d6cc5bcdd889179288b4
GadelhaTI <gadelha.ti@gmail.com>
2023-11-04 01:09:59 -0300

    change border table

--- src/container/template/table.tsx
+++ src/container/template/table.tsx
@@ -14,7 +14,7 @@ export const Table = styled('table', {
     borderRadius: '.3rem',
     display: 'block',
     borderCollapse: 'collapse',
-    border: 'solid $two1 1px',
+    // border: 'solid $two1 1px',
     boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
     'thead > tr:first-child': {
         fontWeight: 'bold',
