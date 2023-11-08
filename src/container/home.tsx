fcab65784d9803fdc88cd2c2496ff9e18822e9d1
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-11-08 12:58:56 -0300

    add cards at home

--- src/container/home.tsx
+++ src/container/home.tsx
@@ -1,5 +1,9 @@
+import { Icon } from '../assets/svg.access';
 import { getPayload, getRoles } from '../service/service.token'
+import { Card, CardContainer } from './template/card';
+import { SideTitle } from './template/flex';
 import { Header, TitleHeader } from './template/header'
+import { Tooltip } from './tooltip/tooltip';
 
 export const Home = () => {
 
@@ -9,6 +13,23 @@ export const Home = () => {
                 <TitleHeader>Home<h1>{getPayload().sub}</h1></TitleHeader>
                 <p>{getRoles()}</p>
             </Header>
+            <CardContainer>
+                <Card>
+                    <h2><a href={`#/`} ><Icon name="collection"/><p>Home</p></a></h2>
+                </Card>
+                <Card>
+                    <h2><a href={`#/food`} ><Icon name="collection"/><p>Food</p></a></h2>
+                </Card>
+                <Card>
+                    <h2><a href={`#/food_category`} ><Icon name="collection"/><p>Food Category</p></a></h2>
+                </Card>
+                <Card>
+                    <h2><a href={`#/user`} ><Icon name="collection"/><p>User</p></a></h2>
+                </Card>
+                <Card>
+                    <h2><a href={`#/role`} ><Icon name="collection"/><p>Role</p></a></h2>
+                </Card>
+            </CardContainer>
         </>
     );
 }
