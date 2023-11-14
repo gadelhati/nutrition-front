00334482c826e9204ce023fb10ef6e9bacd15bc8
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-11-14 17:02:11 -0300

    change height of inputs

--- src/container/form/generic.field.tsx
+++ src/container/form/generic.field.tsx
@@ -26,7 +26,7 @@ export const Container = styled('div',{
                 alignItems: 'center',
             },
             inputs: {
-                minHeight: '13.5rem',
+                height: '13.5rem',
             },
             response: {
                 height: '5rem',
