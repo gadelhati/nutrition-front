04e282435cab66611ce90cbf61198545ff46571e
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-12-01 10:35:54 -0300

    loadsubstatus array

--- src/container/form/generic.form.tsx
+++ src/container/form/generic.form.tsx
@@ -106,7 +106,7 @@ export const GenericForm = <T extends { id: string, name: string }>(object: any)
     const loadSubStates = async () => {
         Object.entries(state).map(([key, value], index) => {
             return (
-                !(atribute[index]?.type === 'checkbox' || atribute[index]?.type === 'date' || value === null && atribute[index].worth === 0 || value === null && atribute[index].worth === '' || atribute[index]?.type !== 'undefined' && !Array.isArray(atribute[index]?.worth)) || atribute[index]?.type === 'object' &&
+                !(atribute[index]?.type === 'checkbox' || atribute[index]?.type === 'date' || value === null && atribute[index].worth === 0 || value === null && atribute[index].worth === '' || atribute[index]?.type !== 'undefined' && !Array.isArray(atribute[index]?.worth)) &&
                 retrieve(key, 0, 1000, '', '').then((data: any) => {
                     startTransition(() => {
                         subStates[index] = data.content
